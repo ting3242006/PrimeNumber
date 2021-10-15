fa6e6ad65e3bc00125de6c2f62afc49d1eb86956
ting3242006 <ting3242006@hotmail.com>
2021-10-15 12:04:56 +0800

    Initial Commit

---
+++ PrimeNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrimeNumber
+//
+//  Created by Ting on 2021/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
